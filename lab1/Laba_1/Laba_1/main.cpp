98fe6e81c4c56a3d53a42ed2a96810a8d6d6dbab
Ilkin-Galoev <ilkinqaloev@gmail.com>
2017-11-26 23:31:12 +0300

    First commit

---
+++ lab1/Laba_1/Laba_1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*---Функция факториала---*/
+int fact(int N)
+{
+    if (N<0)
+        return 0;
+    if (N == 0)
+        return 1;
+    else
+        return N*fact(N-1);
+}
+
+int main() {
+    int N;
+    cout<<"Введите число для вычисления факториала: ";
+    cin>>N;
+    cout << "Факториал для числа " << N << " = " << fact(N) << endl;
+    return 0;
+}
