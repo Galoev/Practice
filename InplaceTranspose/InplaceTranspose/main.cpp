e797577b9f4b111ca08b328fb57c2d612b07374e
Ilkin-Galoev <ilkinqaloev@gmail.com>
2018-06-07 14:42:28 +0300

    Insert function added

--- InplaceTranspose/InplaceTranspose/main.cpp
+++ InplaceTranspose/InplaceTranspose/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    
+using namespace std;
+
+void Insert(float *mas, const unsigned int from, const unsigned int to){
+    float tmp = mas[from];
+    if (from == to ) return;
+    if (from > to)
+        for (size_t i = from; i>to; i--)
+            mas[i] = mas[i-1];
+    else
+        for (size_t i = from; i<to; i++)
+            mas[i] = mas[i+1];
+    mas[to]=tmp;
+}
+
+int main() {
+    size_t n = 3, m = 5;
+    size_t size = n*m;
+    float *mas = new float[size];
+    for (int i = 0; i<size; i++)
+        mas[i] = i;
+    Insert(mas, 5, 1);
+    cout << "[";
+    for(int i = 0; i<n*m; i++){
+        cout << mas[i] << " ";
+    }
+    cout << "]"<<endl;
     return 0;
 }
