4940ea0e037371aec0a1d5a86eaea13ca8977125
Ilkin-Galoev <ilkinqaloev@gmail.com>
2018-06-07 15:34:56 +0300

    Added the first transpose algorithm

--- InplaceTranspose/InplaceTranspose/main.cpp
+++ InplaceTranspose/InplaceTranspose/main.cpp
@@ -14,13 +14,35 @@ void Insert(float *mas, const unsigned int from, const unsigned int to){
     mas[to]=tmp;
 }
 
+void TransformMtrxFirstAlg(float *mtrx, const int m, const int n){
+    int place = 1;
+    int start = m;
+    int end = m;
+    
+    for (int i(0); i < m; i++){
+        for (int j = start; j < (n * m -1); j += end){
+            Insert(mtrx, j, place);
+            place++;
+        }
+        start += (n-1);
+        place++;
+        end--;
+    }
+}
+
 int main() {
     size_t n = 3, m = 5;
     size_t size = n*m;
     float *mas = new float[size];
     for (int i = 0; i<size; i++)
-        mas[i] = i;
-    Insert(mas, 5, 1);
+        mas[i] = i+1;
+    //Insert(mas, 5, 1);
+    cout << "[";
+    for(int i = 0; i<n*m; i++){
+        cout << mas[i] << " ";
+    }
+    cout << "]"<<endl;
+    TransformMtrxFirstAlg(mas, m, n);
     cout << "[";
     for(int i = 0; i<n*m; i++){
         cout << mas[i] << " ";
