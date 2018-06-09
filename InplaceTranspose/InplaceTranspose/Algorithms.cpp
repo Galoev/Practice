f04d852b8dc11a5f099b890588d5ca22d6079911
Ilkin-Galoev <ilkinqaloev@gmail.com>
2018-06-09 10:36:14 +0300

    The width of the indentation was corrected

--- InplaceTranspose/InplaceTranspose/Algorithms.cpp
+++ InplaceTranspose/InplaceTranspose/Algorithms.cpp
@@ -6,112 +6,112 @@
 using namespace std;
 
 bool Insert(float *pMas, const int aFrom, const int aTo){
-    if (aFrom <= 0 || aTo <= 0){
-        return false;
-    }
-    if (aFrom == aTo ) {
-        return true;
-    }
-    float tmp = pMas[aFrom];
-    if (aFrom > aTo)
-        for (int i = aFrom; i>aTo; i--)
-            pMas[i] = pMas[i-1];
-    else
-        for (int i = aFrom; i<aTo; i++)
-            pMas[i] = pMas[i+1];
-    pMas[aTo]=tmp;
+  if (aFrom <= 0 || aTo <= 0){
+    return false;
+  }
+  if (aFrom == aTo ) {
     return true;
+  }
+  float tmp = pMas[aFrom];
+  if (aFrom > aTo)
+    for (int i = aFrom; i>aTo; i--)
+      pMas[i] = pMas[i-1];
+  else
+    for (int i = aFrom; i<aTo; i++)
+      pMas[i] = pMas[i+1];
+  pMas[aTo]=tmp;
+  return true;
 }
 
 bool TransformMtrxFirstAlg(float *pMtrx, const int nRow, const int nCol){
-    if (nRow <= 0 || nCol <= 0){
-        return false;
+  if (nRow <= 0 || nCol <= 0){
+    return false;
+  }
+  int place = 1;
+  int start = nCol;
+  int end = nCol;
+
+  for (int i = 0; i < nCol; i++){
+    for (int j = start; j < (nRow * nCol -1); j += end){
+      Insert(pMtrx, j, place);
+      place++;
     }
-    int place = 1;
-    int start = nCol;
-    int end = nCol;
-    
-    for (int i = 0; i < nCol; i++){
-        for (int j = start; j < (nRow * nCol -1); j += end){
-            Insert(pMtrx, j, place);
-            place++;
-        }
-        start += (nRow-1);
-        place++;
-        end--;
-    }
-    return true;
+    start += (nRow-1);
+    place++;
+    end--;
+  }
+  return true;
 }
 
 bool TransformMtrxSecondAlg(float *pMtrx, const int nRow, const int nCol){
-    if (nRow <= 0 || nCol <= 0){
-        return false;
+  if (nRow <= 0 || nCol <= 0){
+    return false;
+  }
+  for (int i = 1; i < nRow ; i++){
+    for (int j = 0; j < nCol ; j++){
+      Insert(pMtrx, i * nCol + j, j * (i + 1) + i);
     }
-    for (int i = 1; i < nRow ; i++){
-        for (int j = 0; j < nCol ; j++){
-            Insert(pMtrx, i * nCol + j, j * (i + 1) + i);
-        }
-    }
-    return true;
-}
+  }
+  return true;
+  }
 
-bool TransformMtrxThirdAlg(float *pMtrx, int nRow, int nCol){
-    if (nRow <= 0 || nCol <= 0){
-        return false;
+  bool TransformMtrxThirdAlg(float *pMtrx, int nRow, int nCol){
+  if (nRow <= 0 || nCol <= 0){
+    return false;
+  }
+  while(nCol>1)
+  {
+    for(unsigned int i=0;i<nRow;i++) {
+      unsigned int index=nCol-1+nCol*(nRow-1-i);
+      unsigned int target=nCol*nRow-1-i;
+      Insert(pMtrx, index, target);
     }
-    while(nCol>1)
-    {
-        for(unsigned int i=0;i<nRow;i++) {
-            unsigned int index=nCol-1+nCol*(nRow-1-i);
-            unsigned int target=nCol*nRow-1-i;
-            Insert(pMtrx, index, target);
-        }
-        --nCol;
-    }
-    return true;
+    --nCol;
+  }
+  return true;
 }
 
 bool TransformMtrxFourthAlg(float *pMtrx, const int nRow, const int nCol){
-    if (nRow <= 0 || nCol <= 0){
-        return false;
-    }
-    int size = nRow*nCol - 1;
-    float t;
-    int next;
-    int cycleBegin;
-    int i;
-    vector<bool> b(nRow*nCol);
-    
-    for (size_t i = 0; i<nRow*nCol; i++)
-        b[i] = false;
-    b[0] = b[size] = 1;
-    i = 1;
-    while (i < size)
+  if (nRow <= 0 || nCol <= 0){
+    return false;
+  }
+  int size = nRow*nCol - 1;
+  float t;
+  int next;
+  int cycleBegin;
+  int i;
+  vector<bool> b(nRow*nCol);
+
+  for (size_t i = 0; i<nRow*nCol; i++)
+    b[i] = false;
+  b[0] = b[size] = 1;
+  i = 1;
+  while (i < size)
+  {
+    cycleBegin = i;
+    t = pMtrx[i];
+    do
     {
-        cycleBegin = i;
-        t = pMtrx[i];
-        do
-        {
-            next = (i*nRow)%size;
-            swap(pMtrx[next], t);
-            b[i] = 1;
-            i = next;
-        }
-        while (i != cycleBegin);
-        
-        for (i = 1; i < size && b[i]; i++);
+      next = (i*nRow)%size;
+      swap(pMtrx[next], t);
+      b[i] = 1;
+      i = next;
     }
-    return true;
+    while (i != cycleBegin);
+    
+    for (i = 1; i < size && b[i]; i++);
+  }
+  return true;
 }
 
 bool CheckTranspose(float *pA, float *pB, const unsigned int nSize){
-    if (nSize <= 0){
-        return false;
+  if (nSize <= 0){
+    return false;
+  }
+  for (int i=0; i<nSize; i++){
+    if (pA[i] != pB[i]){
+      return false;
     }
-    for (int i=0; i<nSize; i++){
-        if (pA[i] != pB[i]){
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
--- InplaceTranspose/InplaceTranspose/main.cpp
+++ InplaceTranspose/InplaceTranspose/main.cpp
@@ -11,185 +11,185 @@ using namespace std;
 #define TAP_COMPILE
 #include "/Users/ilkin_galoev/Downloads/tst_example (1)/tst_example/libtap/cpp_tap.h"
 int main(int, char *[]) {
-    using namespace std;
-    size_t n1 = 2, m1 = 4, n2 = 5, m2 = 3, n3 = 1, m3 = 2, n4 = 1, m4 = 1, n5 = 8, m5 = 1;
-    size_t size1 = n1*m1, size2 = n2*m2, size3 = n3*m3, size4 = n4*m4, size5 = n5*m5;
-    float *mas1 = new float[size1];
-    for (int i = 0; i<size1; i++)
-        mas1[i] = i+1;
-    float *mas2 = new float[size2];
-    for (int i = 0; i<size2; i++)
-        mas2[i] = i+1;
-    float *mas3 = new float[size3];
-    for (int i = 0; i<size3; i++)
-        mas3[i] = i+1;
-    float *mas4 = new float[size4];
-    for (int i = 0; i<size4; i++)
-        mas4[i] = i+1;
-    float *mas5 = new float[size5];
-    for (int i = 0; i<size5; i++)
-        mas5[i] = i+1;
-    float rightMas1 [] = {1, 5, 2, 6, 3, 7, 4, 8};
-    float rightMas2 [] = {1, 4, 7, 10, 13, 2, 5, 8, 11, 14, 3, 6, 9, 12, 15};
-    float rightMas3 [] = {1, 2};
-    float rightMas4 [] = {1};
-    float rightMas5 [] = {1, 2, 3, 4, 5, 6, 7, 8};
-    float *falseMas1 = new float[size1];
-    for (int i = 0; i<size1; i++)
-        falseMas1[i] = 0;
-    float *falseMas2 = new float[size2];
-    for (int i = 0; i<size2; i++)
-        falseMas2[i] = 0;
-    float falseMas3 [] = {0, 0};
-    float falseMas4 [] = {0};
-    float *falseMas5 = new float[size5];
-    for (int i = 0; i<size5; i++)
-        falseMas5[i] = 0;
-    
-    plan_tests(66);
-    
-    TransformMtrxFirstAlg(mas1, n1, m1);
-    TransformMtrxFirstAlg(mas2, n2, m2);
-    TransformMtrxFirstAlg(mas3, n3, m3);
-    TransformMtrxFirstAlg(mas4, n4, m4);
-    TransformMtrxFirstAlg(mas5, n5, m5);
-    ok1(CheckTranspose(mas1, rightMas1, size1) == true);
-    ok1(CheckTranspose(mas1, falseMas1, size1) == false);
-    ok1(CheckTranspose(mas2, rightMas2, size2) == true);
-    ok1(CheckTranspose(mas2, falseMas2, size2) == false);
-    ok1(CheckTranspose(mas3, rightMas3, size3) == true);
-    ok1(CheckTranspose(mas3, falseMas3, size3) == false);
-    ok1(CheckTranspose(mas4, rightMas4, size4) == true);
-    ok1(CheckTranspose(mas4, falseMas4, size4) == false);
-    ok1(CheckTranspose(mas5, rightMas5, size5) == true);
-    ok1(CheckTranspose(mas5, falseMas5, size5) == false);
-    ok1(TransformMtrxFirstAlg(mas1, -5, -10) == false);
-    ok1(TransformMtrxFirstAlg(mas1, 0, 0) == false);
-    ok1(TransformMtrxFirstAlg(mas1, 0, 1) == false);
-    ok1(TransformMtrxFirstAlg(mas1, 1, 0) == false);
-    
-    for (int i = 0; i<size1; i++)
-        mas1[i] = i+1;
-    for (int i = 0; i<size2; i++)
-        mas2[i] = i+1;
-    for (int i = 0; i<size3; i++)
-        mas3[i] = i+1;
-    mas4[0] = 1;
-    for (int i = 0; i<size5; i++)
-        mas5[i] = i+1;
-    TransformMtrxSecondAlg(mas1, n1, m1);
-    TransformMtrxSecondAlg(mas2, n2, m2);
-    TransformMtrxSecondAlg(mas3, n3, m3);
-    TransformMtrxSecondAlg(mas4, n4, m4);
-    TransformMtrxSecondAlg(mas5, n5, m5);
-    ok1(CheckTranspose(mas1, rightMas1, size1) == true);
-    ok1(CheckTranspose(mas1, falseMas1, size1) == false);
-    ok1(CheckTranspose(mas2, rightMas2, size2) == true);
-    ok1(CheckTranspose(mas2, falseMas2, size2) == false);
-    ok1(CheckTranspose(mas3, rightMas3, size3) == true);
-    ok1(CheckTranspose(mas3, falseMas3, size3) == false);
-    ok1(CheckTranspose(mas4, rightMas4, size4) == true);
-    ok1(CheckTranspose(mas4, falseMas4, size4) == false);
-    ok1(CheckTranspose(mas5, rightMas5, size5) == true);
-    ok1(CheckTranspose(mas5, falseMas5, size5) == false);
-    ok1(TransformMtrxSecondAlg(mas1, -5, -10) == false);
-    ok1(TransformMtrxSecondAlg(mas1, 0, 0) == false);
-    ok1(TransformMtrxSecondAlg(mas1, 0, 1) == false);
-    ok1(TransformMtrxSecondAlg(mas1, 1, 0) == false);
-    
-    
-    for (int i = 0; i<size1; i++)
-        mas1[i] = i+1;
-    for (int i = 0; i<size2; i++)
-        mas2[i] = i+1;
-    for (int i = 0; i<size3; i++)
-        mas3[i] = i+1;
-    mas4[0] = 1;
-    for (int i = 0; i<size5; i++)
-        mas5[i] = i+1;
-    TransformMtrxThirdAlg(mas1, n1, m1);
-    TransformMtrxThirdAlg(mas2, n2, m2);
-    TransformMtrxThirdAlg(mas3, n3, m3);
-    TransformMtrxThirdAlg(mas4, n4, m4);
-    TransformMtrxThirdAlg(mas5, n5, m5);
-    ok1(CheckTranspose(mas1, rightMas1, size1) == true);
-    ok1(CheckTranspose(mas1, falseMas1, size1) == false);
-    ok1(CheckTranspose(mas2, rightMas2, size2) == true);
-    ok1(CheckTranspose(mas2, falseMas2, size2) == false);
-    ok1(CheckTranspose(mas3, rightMas3, size3) == true);
-    ok1(CheckTranspose(mas3, falseMas3, size3) == false);
-    ok1(CheckTranspose(mas4, rightMas4, size4) == true);
-    ok1(CheckTranspose(mas4, falseMas4, size4) == false);
-    ok1(CheckTranspose(mas5, rightMas5, size5) == true);
-    ok1(CheckTranspose(mas5, falseMas5, size5) == false);
-    ok1(TransformMtrxThirdAlg(mas1, -5, -10) == false);
-    ok1(TransformMtrxThirdAlg(mas1, 0, 0) == false);
-    ok1(TransformMtrxThirdAlg(mas1, 0, 1) == false);
-    ok1(TransformMtrxThirdAlg(mas1, 1, 0) == false);
-    
-    for (int i = 0; i<size1; i++)
-        mas1[i] = i+1;
-    for (int i = 0; i<size2; i++)
-        mas2[i] = i+1;
-    for (int i = 0; i<size3; i++)
-        mas3[i] = i+1;
-    mas4[0] = 1;
-    for (int i = 0; i<size5; i++)
-        mas5[i] = i+1;
-    TransformMtrxFourthAlg(mas1, n1, m1);
-    TransformMtrxFourthAlg(mas2, n2, m2);
-    TransformMtrxFourthAlg(mas3, n3, m3);
-    TransformMtrxFourthAlg(mas4, n4, m4);
-    TransformMtrxFourthAlg(mas5, n5, m5);
-    ok1(CheckTranspose(mas1, rightMas1, size1) == true);
-    ok1(CheckTranspose(mas1, falseMas1, size1) == false);
-    ok1(CheckTranspose(mas2, rightMas2, size2) == true);
-    ok1(CheckTranspose(mas2, falseMas2, size2) == false);
-    ok1(CheckTranspose(mas3, rightMas3, size3) == true);
-    ok1(CheckTranspose(mas3, falseMas3, size3) == false);
-    ok1(CheckTranspose(mas4, rightMas4, size4) == true);
-    ok1(CheckTranspose(mas4, falseMas4, size4) == false);
-    ok1(CheckTranspose(mas5, rightMas5, size5) == true);
-    ok1(CheckTranspose(mas5, falseMas5, size5) == false);
-    ok1(TransformMtrxFourthAlg(mas1, -5, -10) == false);
-    ok1(TransformMtrxFourthAlg(mas1, 0, 0) == false);
-    ok1(TransformMtrxFourthAlg(mas1, 0, 1) == false);
-    ok1(TransformMtrxFourthAlg(mas1, 1, 0) == false);
-    
-    ok1(CheckTranspose(mas1, rightMas1, size1) == true);
-    ok1(CheckTranspose(mas1, falseMas1, size1) == false);
-    ok1(CheckTranspose(mas1, rightMas1, 0) == false);
-    ok1(Insert(mas1, -8, 0) == false);
-    ok1(Insert(mas1, -8, -1) == false);
-    ok1(Insert(mas1, 0, -1) == false);
-    ok1(Insert(mas1, 0, 0) == false);
-    ok1(Insert(mas1, 0, 1) == false);
-    ok1(Insert(mas1, 1, 0) == false);
-    ok1(Insert(mas1, 1, 1));
-    
-    return exit_status();
-    
-    return 0;
+  using namespace std;
+  size_t n1 = 2, m1 = 4, n2 = 5, m2 = 3, n3 = 1, m3 = 2, n4 = 1, m4 = 1, n5 = 8, m5 = 1;
+  size_t size1 = n1*m1, size2 = n2*m2, size3 = n3*m3, size4 = n4*m4, size5 = n5*m5;
+  float *mas1 = new float[size1];
+  for (int i = 0; i<size1; i++)
+    mas1[i] = i+1;
+  float *mas2 = new float[size2];
+  for (int i = 0; i<size2; i++)
+    mas2[i] = i+1;
+  float *mas3 = new float[size3];
+  for (int i = 0; i<size3; i++)
+    mas3[i] = i+1;
+  float *mas4 = new float[size4];
+  for (int i = 0; i<size4; i++)
+    mas4[i] = i+1;
+  float *mas5 = new float[size5];
+  for (int i = 0; i<size5; i++)
+    mas5[i] = i+1;
+  float rightMas1 [] = {1, 5, 2, 6, 3, 7, 4, 8};
+  float rightMas2 [] = {1, 4, 7, 10, 13, 2, 5, 8, 11, 14, 3, 6, 9, 12, 15};
+  float rightMas3 [] = {1, 2};
+  float rightMas4 [] = {1};
+  float rightMas5 [] = {1, 2, 3, 4, 5, 6, 7, 8};
+  float *falseMas1 = new float[size1];
+  for (int i = 0; i<size1; i++)
+    falseMas1[i] = 0;
+  float *falseMas2 = new float[size2];
+  for (int i = 0; i<size2; i++)
+    falseMas2[i] = 0;
+  float falseMas3 [] = {0, 0};
+  float falseMas4 [] = {0};
+  float *falseMas5 = new float[size5];
+  for (int i = 0; i<size5; i++)
+    falseMas5[i] = 0;
+  
+  plan_tests(66);
+  
+  TransformMtrxFirstAlg(mas1, n1, m1);
+  TransformMtrxFirstAlg(mas2, n2, m2);
+  TransformMtrxFirstAlg(mas3, n3, m3);
+  TransformMtrxFirstAlg(mas4, n4, m4);
+  TransformMtrxFirstAlg(mas5, n5, m5);
+  ok1(CheckTranspose(mas1, rightMas1, size1) == true);
+  ok1(CheckTranspose(mas1, falseMas1, size1) == false);
+  ok1(CheckTranspose(mas2, rightMas2, size2) == true);
+  ok1(CheckTranspose(mas2, falseMas2, size2) == false);
+  ok1(CheckTranspose(mas3, rightMas3, size3) == true);
+  ok1(CheckTranspose(mas3, falseMas3, size3) == false);
+  ok1(CheckTranspose(mas4, rightMas4, size4) == true);
+  ok1(CheckTranspose(mas4, falseMas4, size4) == false);
+  ok1(CheckTranspose(mas5, rightMas5, size5) == true);
+  ok1(CheckTranspose(mas5, falseMas5, size5) == false);
+  ok1(TransformMtrxFirstAlg(mas1, -5, -10) == false);
+  ok1(TransformMtrxFirstAlg(mas1, 0, 0) == false);
+  ok1(TransformMtrxFirstAlg(mas1, 0, 1) == false);
+  ok1(TransformMtrxFirstAlg(mas1, 1, 0) == false);
+  
+  for (int i = 0; i<size1; i++)
+    mas1[i] = i+1;
+  for (int i = 0; i<size2; i++)
+    mas2[i] = i+1;
+  for (int i = 0; i<size3; i++)
+    mas3[i] = i+1;
+  mas4[0] = 1;
+  for (int i = 0; i<size5; i++)
+    mas5[i] = i+1;
+  TransformMtrxSecondAlg(mas1, n1, m1);
+  TransformMtrxSecondAlg(mas2, n2, m2);
+  TransformMtrxSecondAlg(mas3, n3, m3);
+  TransformMtrxSecondAlg(mas4, n4, m4);
+  TransformMtrxSecondAlg(mas5, n5, m5);
+  ok1(CheckTranspose(mas1, rightMas1, size1) == true);
+  ok1(CheckTranspose(mas1, falseMas1, size1) == false);
+  ok1(CheckTranspose(mas2, rightMas2, size2) == true);
+  ok1(CheckTranspose(mas2, falseMas2, size2) == false);
+  ok1(CheckTranspose(mas3, rightMas3, size3) == true);
+  ok1(CheckTranspose(mas3, falseMas3, size3) == false);
+  ok1(CheckTranspose(mas4, rightMas4, size4) == true);
+  ok1(CheckTranspose(mas4, falseMas4, size4) == false);
+  ok1(CheckTranspose(mas5, rightMas5, size5) == true);
+  ok1(CheckTranspose(mas5, falseMas5, size5) == false);
+  ok1(TransformMtrxSecondAlg(mas1, -5, -10) == false);
+  ok1(TransformMtrxSecondAlg(mas1, 0, 0) == false);
+  ok1(TransformMtrxSecondAlg(mas1, 0, 1) == false);
+  ok1(TransformMtrxSecondAlg(mas1, 1, 0) == false);
+  
+  
+  for (int i = 0; i<size1; i++)
+    mas1[i] = i+1;
+  for (int i = 0; i<size2; i++)
+    mas2[i] = i+1;
+  for (int i = 0; i<size3; i++)
+    mas3[i] = i+1;
+  mas4[0] = 1;
+  for (int i = 0; i<size5; i++)
+    mas5[i] = i+1;
+  TransformMtrxThirdAlg(mas1, n1, m1);
+  TransformMtrxThirdAlg(mas2, n2, m2);
+  TransformMtrxThirdAlg(mas3, n3, m3);
+  TransformMtrxThirdAlg(mas4, n4, m4);
+  TransformMtrxThirdAlg(mas5, n5, m5);
+  ok1(CheckTranspose(mas1, rightMas1, size1) == true);
+  ok1(CheckTranspose(mas1, falseMas1, size1) == false);
+  ok1(CheckTranspose(mas2, rightMas2, size2) == true);
+  ok1(CheckTranspose(mas2, falseMas2, size2) == false);
+  ok1(CheckTranspose(mas3, rightMas3, size3) == true);
+  ok1(CheckTranspose(mas3, falseMas3, size3) == false);
+  ok1(CheckTranspose(mas4, rightMas4, size4) == true);
+  ok1(CheckTranspose(mas4, falseMas4, size4) == false);
+  ok1(CheckTranspose(mas5, rightMas5, size5) == true);
+  ok1(CheckTranspose(mas5, falseMas5, size5) == false);
+  ok1(TransformMtrxThirdAlg(mas1, -5, -10) == false);
+  ok1(TransformMtrxThirdAlg(mas1, 0, 0) == false);
+  ok1(TransformMtrxThirdAlg(mas1, 0, 1) == false);
+  ok1(TransformMtrxThirdAlg(mas1, 1, 0) == false);
+  
+  for (int i = 0; i<size1; i++)
+    mas1[i] = i+1;
+  for (int i = 0; i<size2; i++)
+    mas2[i] = i+1;
+  for (int i = 0; i<size3; i++)
+    mas3[i] = i+1;
+  mas4[0] = 1;
+  for (int i = 0; i<size5; i++)
+    mas5[i] = i+1;
+  TransformMtrxFourthAlg(mas1, n1, m1);
+  TransformMtrxFourthAlg(mas2, n2, m2);
+  TransformMtrxFourthAlg(mas3, n3, m3);
+  TransformMtrxFourthAlg(mas4, n4, m4);
+  TransformMtrxFourthAlg(mas5, n5, m5);
+  ok1(CheckTranspose(mas1, rightMas1, size1) == true);
+  ok1(CheckTranspose(mas1, falseMas1, size1) == false);
+  ok1(CheckTranspose(mas2, rightMas2, size2) == true);
+  ok1(CheckTranspose(mas2, falseMas2, size2) == false);
+  ok1(CheckTranspose(mas3, rightMas3, size3) == true);
+  ok1(CheckTranspose(mas3, falseMas3, size3) == false);
+  ok1(CheckTranspose(mas4, rightMas4, size4) == true);
+  ok1(CheckTranspose(mas4, falseMas4, size4) == false);
+  ok1(CheckTranspose(mas5, rightMas5, size5) == true);
+  ok1(CheckTranspose(mas5, falseMas5, size5) == false);
+  ok1(TransformMtrxFourthAlg(mas1, -5, -10) == false);
+  ok1(TransformMtrxFourthAlg(mas1, 0, 0) == false);
+  ok1(TransformMtrxFourthAlg(mas1, 0, 1) == false);
+  ok1(TransformMtrxFourthAlg(mas1, 1, 0) == false);
+  
+  ok1(CheckTranspose(mas1, rightMas1, size1) == true);
+  ok1(CheckTranspose(mas1, falseMas1, size1) == false);
+  ok1(CheckTranspose(mas1, rightMas1, 0) == false);
+  ok1(Insert(mas1, -8, 0) == false);
+  ok1(Insert(mas1, -8, -1) == false);
+  ok1(Insert(mas1, 0, -1) == false);
+  ok1(Insert(mas1, 0, 0) == false);
+  ok1(Insert(mas1, 0, 1) == false);
+  ok1(Insert(mas1, 1, 0) == false);
+  ok1(Insert(mas1, 1, 1));
+  
+  return exit_status();
+  
+  return 0;
 }
 #else
 int main() {
-    size_t n = 8, m = 1;
-    size_t size = n*m;
-    float *mas = new float[size];
-    for (int i = 0; i<size; i++)
-        mas[i] = i+1;
-    //Insert(mas, 5, 1);
-    cout << "[";
-    for(int i = 0; i<n*m; i++){
-        cout << mas[i] << " ";
-    }
-    cout << "]"<<endl;
-    TransformMtrxFourthAlg(mas, n, m);
-    cout << "[";
-    for(int i = 0; i<n*m; i++){
-        cout << mas[i] << " ";
-    }
-    cout << "]"<<endl;
-    return 0;
+  size_t n = 8, m = 1;
+  size_t size = n*m;
+  float *mas = new float[size];
+  for (int i = 0; i<size; i++)
+    mas[i] = i+1;
+  //Insert(mas, 5, 1);
+  cout << "[";
+  for(int i = 0; i<n*m; i++){
+    cout << mas[i] << " ";
+  }
+  cout << "]"<<endl;
+  TransformMtrxFourthAlg(mas, n, m);
+  cout << "[";
+  for(int i = 0; i<n*m; i++){
+    cout << mas[i] << " ";
+  }
+  cout << "]"<<endl;
+  return 0;
 }
 #endif
